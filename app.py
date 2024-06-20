7bcb15d84c95fe3a6a7b1d1bf82e5da09b9042b6
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-06-20 08:00:16 +0530

    ALgo-Ai final Model

--- app.py
+++ app.py
@@ -1,87 +1,68 @@
 import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
-import google.generativeai as genai
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings, ChatNVIDIA
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+import time
+
 from dotenv import load_dotenv
-import tempfile
 load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+## load the Nvidia API key
+os.environ['NVIDIA_API_KEY']=os.getenv("NVIDIA_API_KEY")
 
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
+def vector_embedding():
 
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_local")
+    if "vectors" not in st.session_state:
 
-def get_conversational_chain():
-    prompt_template = """Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not provided in the context, just say, "answer is not available in the context", don't provide the wrong answer \n\n
-    Context:\n{context}\n
-    Question:\n{question}\n
-    
-    Answer:
-    """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
-    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-    return chain
+        st.session_state.embeddings=NVIDIAEmbeddings()
+        st.session_state.loader=PyPDFDirectoryLoader("/Users/hemantkumar/Developer/ntcc code/Algo/documents") ## Data Ingestion
+        st.session_state.docs=st.session_state.loader.load() ## Document Loading
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=700,chunk_overlap=50) ## Chunk Creation
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:30]) #splitting
+        print("hEllo")
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings) #vector OpenAI embeddings
 
-def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    try:
-        new_db = FAISS.load_local("faiss_local", embeddings, allow_dangerous_deserialization=True)
-        docs = new_db.similarity_search(user_question)
-        chain = get_conversational_chain()
-        response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
-        st.write("Reply : ", response["output_text"])
-    except FileNotFoundError:
-        st.error("Faiss index file not found. Please check if the file exists.")
-        st.stop()
+st.title("Algo-AI")
+llm = ChatNVIDIA(model="meta/llama3-70b-instruct")
 
-def main():
-    st.set_page_config("Algo-AI")
-    st.header("Chat with Algo-AI")
 
-    user_question = st.text_input("Ask a Question from the Pdf files")
+prompt=ChatPromptTemplate.from_template(
+"""
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
 
-    if user_question:
-        user_input(user_question)
-    
-    with st.sidebar:
-        st.title("Menu :")
-        pdf_file = st.file_uploader("Upload your pdf files and click on the Submit Button & Process")
-        if st.button("Submit & Process"):
-            with st.spinner("Processing ...."):
-                if pdf_file is not None:
-                    # Save the uploaded PDF file to a temporary location
-                    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-                        tmp_file.write(pdf_file.read())
-                        pdf_path = tmp_file.name
+"""
+)
 
-                    raw_text = get_pdf_text([pdf_path])  # Pass the file path as a list
-                    text_chunks = get_text_chunks(raw_text)
-                    get_vector_store(text_chunks)
-                    st.success("Done")
 
-                    # Remove the temporary file after processing
-                    os.unlink(pdf_path)
+prompt1=st.text_input("Enter Your Query")
 
-if __name__ == "__main__":
-    main()
+
+if st.button("Start Database"):
+    vector_embedding()
+    st.write("Algo-AI Is Ready")
+
+import time
+
+
+if prompt1:
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    retriever=st.session_state.vectors.as_retriever()
+    retrieval_chain=create_retrieval_chain(retriever,document_chain)
+    start=time.process_time()
+    response=retrieval_chain.invoke({'input':prompt1})
+    print("Response time :",time.process_time()-start)
+    st.write(response['answer'])
+
